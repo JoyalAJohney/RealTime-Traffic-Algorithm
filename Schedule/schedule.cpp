023cfa91628740f9c1e749e195e344110e5c1e0b
Joyal A Johney <joyalajohny@gmail.com>
2019-07-19 23:01:51 +0530

    Threads created for lanes

--- Schedule/schedule.cpp
+++ Schedule/schedule.cpp
@@ -1,2 +1,10 @@
 // use display function to display traffic light
 // then create a separate thread to run for some 5000ms 
+
+#include <stdio.h>
+
+void schedule()
+{
+    int i=1;
+    printf("Lane %d\n",i);
+}
--- main.cpp
+++ main.cpp
@@ -1,13 +1,16 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <chrono>
+#include <thread>
 #include <bits/stdc++.h> 
 #include "class/lane.cpp"
+#include "Schedule/schedule.cpp"
 
 using namespace std;
 
 int main()
 {
     // Reads density array from file
-    float density[] = {20,100,30,50};
+    float density[] = {20,10,30,50};
     int n = sizeof(density)/sizeof(density[0]);
 
     // Sort array
@@ -21,10 +24,15 @@ int main()
         lane[i++].setDensity(density);
     }
 
+
     // Schedule each lane (optimize)
     for(i=0;i<n;i++)
     {
         // schedule(lane[i].getDuration());
+        // New Thread to perform schedule
+        thread scheduleThread(schedule);
+        scheduleThread.join();
+        this_thread::sleep_for(chrono::milliseconds(5000));
     }
 
 
