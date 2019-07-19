2ea57db3f3a8d4c253a48c2fcfd88fbbca547f98
Joyal A Johney <joyalajohny@gmail.com>
2019-07-19 13:17:00 +0530

    File import issue fixed

--- class/lane.cpp
+++ class/lane.cpp
@@ -1,20 +1,21 @@
-#include<stdio.h>
-#include "lane.h"
-
-// Implementation of class Lane
-void Lane::setDensity(float density)
+class Lane 
 {
-    this->density = density;
-    this->duration = setDuration(this->density);
-}
+    private:
+    float density,duration;
 
-float Lane::setDuration(float density)
-{
-    // Equation to convert density to appropriate duration (for now)
-    return (float)(0.6*(density) + 20);
-}
-
-float Lane::getDensity()
-{
-    return this->density;
-}
+    public:
+    void setDensity(float density)
+    {
+        this->density = density;
+        this->duration = setDuration(this->density);
+    }
+    float setDuration(float density)
+    {
+        // Equation to convert density to appropriate duration (for now)
+        return (float)(0.6*(density) + 20);
+    }
+    float getDensity()
+    {
+        return this->density;
+    }
+};
--- main.cpp
+++ main.cpp
@@ -1,39 +1,12 @@
 #include<stdio.h>
 #include<iostream>
-#include "class/lane.h"
+#include "class/lane.cpp"
 
 using namespace std;
-
-
-// // Class representing each lane
-// class Lane{
-
-//     private:
-//     float density,duration;
-
-//     public:
-//     void setDensity(float density)
-//     {
-//         this->density = density;
-//         this->duration = setDuration(this->density);
-//     }
-//     float setDuration(float density){
-//         // Equation to convert density to appropriate duration (for now)
-//         return (float)(0.6*(density) + 20);
-//     }
-
-// };
 
 int main()
 {
     // Reads density array from file
     float density[] = {20,100,30,50};
-    Lane lanes[sizeof(density)/sizeof(density[0])];
-
-    printf("workking\n");
     Lane a;
-    // a.setDensity(20);
-    // printf("%f\n",a.getDensity());
-    a.setDensity(20);
-
 }
